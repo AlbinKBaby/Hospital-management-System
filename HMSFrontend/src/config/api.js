9121a5fdb632b1ec77366e317a913a902264b681
Albin K Baby <albinbaby06@gmail.com>
2025-10-01 15:45:25 +0530

    edited

--- HMSFrontend/src/config/api.js
+++ HMSFrontend/src/config/api.js
@@ -12,11 +12,11 @@ const api = axios.create({
 // Request interceptor to add auth token
 api.interceptors.request.use(
   (config) => {
+    return config;
     const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    return config;
   },
   (error) => {
     return Promise.reject(error);
